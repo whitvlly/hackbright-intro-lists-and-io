2a9ea43bdd990c9f3db72986eeca5bbcec287e8c
Whitney&Gulmira <W&G@msn.com>
2015-11-16 20:09:06 -0800

    food different

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -4,3 +4,10 @@ whitney_favs = whit.readlines()
 gulmira_favs = gul.readlines()
 print whitney_favs
 print gulmira_favs
+whit.close()
+gul.close()
+def compare_favs(whitney_favs, gulmira_favs):
+	if (whitney_favs == gulmira_favs):
+		print "Our favorite foods are the same!"
+	print "Our foods are different"
+compare_favs(whitney_favs, gulmira_favs)
